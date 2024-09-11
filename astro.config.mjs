50f2653a48e5cecec44186b11ee6012483d9ec87
Tyler Hancock <darklime@live.ca>
2024-09-10 19:22:49 -0600

    Remove astro images plugin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import {defineConfig} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image()]
+  integrations: [tailwind(), mdx()]
 });
